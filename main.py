ca8b423e5e8e113263b75afdb0e7d3daf42af9d3
Eisuke15 <hideyu41691@gmail.com>
2022-06-18 17:29:58 +0900

    train normally

---
+++ main.py
@@ -0,0 +1,83 @@
+from argparse import ArgumentParser
+from random import random
+
+import torch
+from torch.optim import Adam
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+from torchvision.datasets import MNIST
+
+from net import VAE
+from utils import mkdir_if_not_exists
+
+parser = ArgumentParser()
+parser.add_argument('--nepoch', type=int, help="number of epochs to train for", default=25)
+parser.add_argument('--bs', type=int, help="input batch size", default=64)
+parser.add_argument('--nz', type=int, help='size of the latent z vector', default=100)
+args = parser.parse_args()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Lambda(lambda x: x.view(-1))])
+
+root = mkdir_if_not_exists('./data')
+dataset_train_valid = MNIST(root=root, train=True, download=True, transform=transform)
+dataset_test = MNIST(root=root, train=False, download=True, transform=transform)
+total_train_data_num = len(dataset_train_valid)
+val_data_num = int(total_train_data_num * 0.3)
+
+dataset_train, dataset_valid = random_split(dataset_train_valid, [total_train_data_num - val_data_num, val_data_num])
+train_dataloader = DataLoader(dataset_train, batch_size=args.bs, shuffle=True, num_workers=2)
+val_dataloader = DataLoader(dataset_valid, batch_size=args.bs, shuffle=False, num_workers=2)
+test_dataloader = DataLoader(dataset_train, batch_size=args.bs, shuffle=False, num_workers=2)
+
+vae = VAE(args.nz)
+vae.to(device)
+optimizer = Adam(vae.parameters())
+
+for epoch in range(args.nepoch):
+    losses = []
+    
+    vae.train()
+
+    for image, label in train_dataloader:
+        image = image.to(device)
+        label = label.to(device)
+
+        optimizer.zero_grad()
+
+        y, _ = vae(image)
+        KL_loss, reconstruction_loss = vae.loss(image)
+        loss = KL_loss + reconstruction_loss
+
+        loss.backward()
+        optimizer.step()
+
+        losses.append(loss.item())
+
+    losses_val = []
+    vae.eval()
+
+    for image, label in val_dataloader:
+        image = image.to(device)
+        label = label.to(device)
+
+        y, _ = vae(image)
+        KL_loss, reconstruction_loss = vae.loss(image)
+        loss = KL_loss + reconstruction_loss
+
+        losses_val.append(loss.detach())
+    
+    print(f'epoch: {epoch + 1}  Train Lower Bound: {sum(losses)/len(losses)}  Valid Lower Bound: {sum(losses_val)/len(losses_val)}')
+        
+
+
+
+
+
+
+
+
