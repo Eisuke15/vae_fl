6189380f4d023fbe468933e0038ee918bec884fc
Eisuke15 <hideyu41691@gmail.com>
2022-06-19 16:51:13 +0900

    refactor central

--- central.py
+++ central.py
@@ -4,12 +4,12 @@ from random import random
 
 import torch
 from torch.optim import Adam
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import MNIST
 
+from common import central_save_net_path, device, mnist_data_root
 from net import VAE
-from common import device, mkdir_if_not_exists
 
 parser = ArgumentParser()
 parser.add_argument('--nepoch', type=int, help="number of epochs to train for", default=25)
@@ -26,17 +26,8 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Lambda(lambda x: x.view(-1))])
 
-root = mkdir_if_not_exists('./data')
-save_net_path = mkdir_if_not_exists('./net/central')
-dataset_train_valid = MNIST(root=root, train=True, download=True, transform=transform)
-dataset_test = MNIST(root=root, train=False, download=True, transform=transform)
-total_train_data_num = len(dataset_train_valid)
-val_data_num = int(total_train_data_num * 0.3)
-
-dataset_train, dataset_valid = random_split(dataset_train_valid, [total_train_data_num - val_data_num, val_data_num])
+dataset_train = MNIST(root=mnist_data_root, train=True, download=True, transform=transform)
 train_dataloader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=2)
-val_dataloader = DataLoader(dataset_valid, batch_size=batch_size, shuffle=False, num_workers=2)
-test_dataloader = DataLoader(dataset_train, batch_size=batch_size, shuffle=False, num_workers=2)
 
 vae = VAE(args.nz, device)
 vae.to(device)
@@ -44,37 +35,21 @@ optimizer = Adam(vae.parameters())
 
 for epoch in range(args.nepoch):
     losses = []
-    
     vae.train()
 
-    for image, label in train_dataloader:
-        image = image.to(device)
-        label = label.to(device)
+    for images, _ in train_dataloader:
+        images = images.to(device)
 
         optimizer.zero_grad()
 
-        y, _ = vae(image)
-        KL_loss, reconstruction_loss = vae.loss(image)
+        KL_loss, reconstruction_loss = vae.loss(images)
         loss = KL_loss + reconstruction_loss
 
         loss.backward()
         optimizer.step()
 
         losses.append(loss.item())
-
-    losses_val = []
-    vae.eval()
-
-    for image, label in val_dataloader:
-        image = image.to(device)
-        label = label.to(device)
-
-        y, _ = vae(image)
-        KL_loss, reconstruction_loss = vae.loss(image)
-        loss = KL_loss + reconstruction_loss
-
-        losses_val.append(loss.detach())
     
-    print(f'epoch: {epoch + 1}  Train Lower Bound: {sum(losses)/len(losses)}  Valid Lower Bound: {sum(losses_val)/len(losses_val)}')
+    print(f'epoch: {epoch + 1}  Train Lower Bound: {sum(losses)/len(losses)}')
 
-    torch.save(vae.state_dict(), os.path.join(save_net_path, f'mnist_vae_nz{args.nz:02d}_e{epoch+1:04d}.pth'))
+    torch.save(vae.state_dict(), os.path.join(central_save_net_path, f'mnist_vae_nz{args.nz:02d}_e{epoch+1:04d}.pth'))
