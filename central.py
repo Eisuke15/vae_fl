77aa536037a53bc90940b7f704dd886c9fb202a0
Eisuke15 <hideyu41691@gmail.com>
2022-07-21 22:04:00 +0900

    refactoring

--- central.py
+++ central.py
@@ -1,43 +1,42 @@
-import os
 from argparse import ArgumentParser
-from random import random
 
 import torch
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import MNIST
+from torchvision.utils import save_image
+from tqdm import tqdm
 
-from common import central_save_net_path, device, mnist_data_root
 from net import VAE
 
 parser = ArgumentParser()
-parser.add_argument('--nepoch', type=int, help="number of epochs to train for", default=25)
+parser.add_argument('--nepoch', type=int, help="number of epochs to train for", default=100)
 parser.add_argument('--nz', type=int, help='size of the latent z vector', default=100)
 parser.add_argument('-g', '--gpu-num', type=int, help='what gpu to use', default=0)
 args = parser.parse_args()
 
-device = device(args.gpu_num)
+device = torch.device(f"cuda:{args.gpu_num}" if torch.cuda.is_available() else "cpu")
 print(device)
 
 batch_size = 64
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Lambda(lambda x: x.view(-1))])
+transform = transforms.ToTensor()
 
-dataset_train = MNIST(root=mnist_data_root, train=True, download=True, transform=transform)
+dataset_train = MNIST(root='data', train=True, download=True, transform=transform)
 train_dataloader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=2)
 
-vae = VAE(args.nz, device)
+vae = VAE(args.nz)
 vae.to(device)
 optimizer = Adam(vae.parameters())
+
+fixed_z = torch.randn(64, args.nz).to(device)
 
 for epoch in range(args.nepoch):
     losses = []
     vae.train()
 
-    for images, _ in train_dataloader:
+    for images, _ in tqdm(train_dataloader, leave=False):
         images = images.to(device)
 
         optimizer.zero_grad()
@@ -50,6 +49,5 @@ for epoch in range(args.nepoch):
 
         losses.append(loss.item())
     
-    print(f'epoch: {epoch + 1}  Train Lower Bound: {sum(losses)/len(losses)}')
-
-    torch.save(vae.state_dict(), os.path.join(central_save_net_path, f'mnist_vae_nz{args.nz:02d}_e{epoch+1:04d}.pth'))
+    print(f'epoch: {epoch}  Train Lower Bound: {sum(losses)/len(losses)}')
+    save_image(vae._decoder(fixed_z), f'images/central/e{epoch}_z{args.nz}.png')
